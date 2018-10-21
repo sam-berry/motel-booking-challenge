044c0c3e66ea0d1cbba850ed42e8a6571c608894
Sam Berry <sberry925@gmail.com>
2018-10-21 22:59:05 +0000

    Add Spring Shell

---
+++ src/main/kotlin/org/samberry/bravochallenge/BravoProgrammingChallengeApplication.kt
@@ -0,0 +1,11 @@
+package org.samberry.bravochallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BravoProgrammingChallengeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BravoProgrammingChallengeApplication>(*args)
+}
