d6ec926e6a165db95b357645ba7be16cc2bb8d35
Sam Berry <sberry925@gmail.com>
2018-10-21 05:03:45 +0000

    Add test that creates multiple rooms

--- src/test/kotlin/org/samberry/bravochallenge/AddNewRoomTest.kt
+++ src/test/kotlin/org/samberry/bravochallenge/AddNewRoomTest.kt
@@ -5,56 +5,93 @@ import org.junit.Before
 import org.junit.Test
 
 class AddNewRoomTest {
+    private lateinit var baseRoom: Room
+
     private lateinit var roomDatabase: MutableMap<String, Room>
     private lateinit var roomService: RoomService
 
     @Before
     fun setUp() {
-        roomDatabase = mutableMapOf()
-        roomService = RoomService(roomDatabase)
-    }
-
-    @Test
-    fun `can add a simple room with 1 bed`() {
-        val room = Room(
+        baseRoom = Room(
             roomNumber = "A123",
             numberOfBeds = 1,
             buildingLevel = 1
         )
 
-        val result = roomService.addRoom(room)
+        roomDatabase = mutableMapOf()
+        roomService = RoomService(roomDatabase)
+    }
 
-        assertThat(result).isEqualTo(room)
-        assertThat(roomDatabase[result.roomNumber]).isEqualTo(result)
+    private fun verifyRoom(actual: Room, expected: Room) {
+        assertThat(actual).isEqualTo(expected)
+        assertThat(roomDatabase[actual.roomNumber]).isEqualTo(expected)
+    }
+
+    @Test
+    fun `can add a single bed room`() {
+        val room = baseRoom.copy(numberOfBeds = 1)
+        val result = roomService.addRoom(room)
+        verifyRoom(result, room)
+    }
+
+    @Test
+    fun `can add a double bed room`() {
+        val room = baseRoom.copy(numberOfBeds = 2)
+        val result = roomService.addRoom(room)
+        verifyRoom(result, room)
+    }
+
+    @Test
+    fun `can add a triple bed room`() {
+        val room = baseRoom.copy(numberOfBeds = 3)
+        val result = roomService.addRoom(room)
+        verifyRoom(result, room)
+    }
+
+    @Test
+    fun `can add a room on the second floor`() {
+        val room = baseRoom.copy(buildingLevel = 2)
+        val result = roomService.addRoom(room)
+        verifyRoom(result, room)
     }
 
     @Test
     fun `can add a handicap accessible room`() {
-        val room = Room(
-            roomNumber = "A123",
-            numberOfBeds = 1,
-            buildingLevel = 1,
-            handicapAccessible = true
-        )
-
+        val room = baseRoom.copy(handicapAccessible = true)
         val result = roomService.addRoom(room)
-
-        assertThat(result).isEqualTo(room)
-        assertThat(roomDatabase[result.roomNumber]).isEqualTo(result)
+        verifyRoom(result, room)
     }
 
     @Test
     fun `can add a pet friendly room`() {
-        val room = Room(
-            roomNumber = "A123",
-            numberOfBeds = 1,
-            buildingLevel = 1,
-            petFriendly = true
+        val room = baseRoom.copy(petFriendly = true)
+        val result = roomService.addRoom(room)
+        verifyRoom(result, room)
+    }
+
+    @Test
+    fun `can add multiple rooms`() {
+        val rooms = setOf(
+            baseRoom.copy(
+                roomNumber = "A345",
+                numberOfBeds = 3
+            ),
+            baseRoom.copy(
+                roomNumber = "B123",
+                buildingLevel = 2
+            ),
+            baseRoom.copy(
+                roomNumber = "C987",
+                numberOfBeds = 2,
+                handicapAccessible = true
+            ),
+            baseRoom.copy(
+                roomNumber = "D543",
+                petFriendly = true
+            )
         )
 
-        val result = roomService.addRoom(room)
-
-        assertThat(result).isEqualTo(room)
-        assertThat(roomDatabase[result.roomNumber]).isEqualTo(result)
+        rooms.forEach { roomService.addRoom(it) }
+        rooms.forEach { assertThat(roomDatabase[it.roomNumber]).isEqualTo(it) }
     }
 }
