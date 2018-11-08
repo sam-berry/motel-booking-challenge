e5d11f1628674d0ab93d747310fc50aa0007ba1e
Sam Berry <sberry925@gmail.com>
2018-11-08 04:39:05 +0000

    Prefer FP for looping logic
    The original intent was to be more performant, but since this serves as a code example I'd rather it use Kotlin functions to simplify the code.

--- src/main/kotlin/org/samberry/motelbooking/service/AvailabilitySearchService.kt
+++ src/main/kotlin/org/samberry/motelbooking/service/AvailabilitySearchService.kt
@@ -1,6 +1,5 @@
 package org.samberry.motelbooking.service
 
-import org.samberry.motelbooking.api.Reservation
 import org.samberry.motelbooking.api.ReservationRequest
 import org.samberry.motelbooking.api.Room
 import org.samberry.motelbooking.dao.ReservationDAO
@@ -19,28 +18,20 @@ class AvailabilitySearchService(
     ): Set<Room> {
         reservationRuleChain.run(reservationRequest)
 
-        val allRooms = roomService.findRooms(reservationRequest)
-        val roomIterator = allRooms.iterator()
+        val availableRooms = roomService.findRooms(reservationRequest)
+            .filter { isRoomAvailable(it, reservationRequest) }
+            .toSet()
 
-        val availableRooms = mutableSetOf<Room>()
-        while (roomIterator.hasNext()) {
-            val room = roomIterator.next()
-            if (isRoomAvailable(room, reservationRequest))
-                availableRooms.add(room)
-        }
-
-        // try to save amenity rooms until they are the only thing left
-        // e.g., if pets aren't coming, try to book a non-pet room
         val roomsExcludingNotNeededAmenities = availableRooms
             .asSequence()
             .filterNot { reservationRequest.numberOfPets == 0 && it.petFriendly }
             .filterNot { !reservationRequest.handicapAccessible && it.handicapAccessible }
             .toSet()
 
-        return if (roomsExcludingNotNeededAmenities.isNotEmpty())
-            roomsExcludingNotNeededAmenities
-        else
+        return if (roomsExcludingNotNeededAmenities.isEmpty())
             availableRooms
+        else
+            roomsExcludingNotNeededAmenities
     }
 
     fun findAnAvailableRoom(
@@ -60,14 +51,7 @@ class AvailabilitySearchService(
         val reservationsForRoom = reservationDAO.findReservationsForRoom(room)
         if (reservationsForRoom.isEmpty()) return true
 
-        val reservationIterator = reservationsForRoom.iterator()
-        var conflictingReservation: Reservation? = null
-        while (conflictingReservation == null && reservationIterator.hasNext()) {
-            val reservation = reservationIterator.next()
-            if (!reservation.endDate.isBefore(startDate) && !reservation.startDate.isAfter(endDate))
-                conflictingReservation = reservation
-        }
-
-        return conflictingReservation == null
+        return !reservationDAO.findReservationsForRoom(room)
+            .any { !startDate.isAfter(it.endDate) && !endDate.isBefore(it.startDate) }
     }
 }
